60da1a6d04f7f56a2054a4ae49803fce91e90280
Sayan Sivakumaran <sivakusayan@gmail.com>
2018-11-27 06:56:04 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, 
+      {
+        use: ['style-loader', 'css-loader', 'sass-loader' ],
+        test: /\.scss$/
+      }
+    ]
+  },
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
+};
