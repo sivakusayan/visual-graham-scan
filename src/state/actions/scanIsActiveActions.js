19f3faf15060ae42525e4a69211922e641ee0f10
Sayan Sivakumaran <sivakusayan@gmail.com>
2018-12-23 08:17:31 -0500

    Added JSDoc to action creators

--- src/state/actions/scanIsActiveActions.js
+++ src/state/actions/scanIsActiveActions.js
@@ -3,10 +3,20 @@ import {
   STOP_SCAN,
 } from '../actionTypes/scanIsActiveActionTypes';
 
+/**
+ * Generates an action to start the Graham Scan.
+ *
+ * @returns {{ type: START_SCAN }}
+ */
 export const startScan = () => ({
   type: START_SCAN,
 });
 
+/**
+ * Generates an action to stop the Graham Scan.
+ * 
+ * @returns {{ type: STOP_SCAN }}
+ */
 export const stopScan = () => ({
   type: STOP_SCAN,
 });
--- src/state/actions/scanStepActions.js
+++ src/state/actions/scanStepActions.js
@@ -6,22 +6,54 @@ import {
   DONE,
 } from '../actionTypes/scanStepActionTypes';
 
+/**
+ * Generates an action to tell the Graham Scan
+ * to find the start point.
+ *
+ * @return {{ type: GET_START_POINT }}
+ */
 export const getStartPoint = () => ({
   type: GET_START_POINT,
 });
 
+/**
+ * Generates an action to tell the Graham Scan to
+ * sort points with respect to their angle with the
+ * start point.
+ *
+ * @return {{ type: SORT_POINTS }}
+ */
 export const sortPoints = () => ({
   type: SORT_POINTS,
 });
 
+/**
+ * Generates an action to tell the Graham Scan
+ * to add the next point to the convex hull.
+ *
+ * @return {{ type: ADD_NEXT_POINT }}
+ */
 export const addNextPoint = () => ({
   type: ADD_NEXT_POINT,
 });
 
+/**
+ * Generates an action to tell the Graham Scan
+ * to fix a right turn that has occured in the
+ * convex hull.
+ *
+ * @return {{ type: FIX_RIGHT_TURN }}
+ */
 export const fixRightTurn = () => ({
   type: FIX_RIGHT_TURN,
 });
 
+/**
+ * Generates an action to let Graham Scan
+ * know that it is done.
+ *
+ * @return {{ type: DONE }}
+ */
 export const done = () => ({
   type: DONE,
 });
