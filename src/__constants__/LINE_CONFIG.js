43dbbddd720051965ab6e7ddfba70656a74a36eb
Sayan Sivakumaran <sivakusayan@gmail.com>
2019-01-23 19:18:59 -0500

    Lines are now bigger on desktop layout. Scaled down driver text to fit on smaller phones. Tweaked scan sort step description.

--- src/__constants__/LINE_CONFIG.js
+++ src/__constants__/LINE_CONFIG.js
@@ -11,6 +11,7 @@ export default {
     if (line.status === ERROR) return '#FF6F59';
     return '#272D2D';
   },
+  strokeWidth: window.innerWidth > 600 ? 4 : 2.3,
   fill: '#272D2D',
   dash: [33, 10],
 };
